38871292ddde676ab99fbc23e944a22e8eb06c79
Mikhail Dudarev <dev@mikhaildudarev.com>
2022-12-13 21:00:49 +0300

    Initial Commit

---
+++ KeychainPoC/Root/KeychainPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainPoCApp.swift
+//  KeychainPoC
+//
+//  Created by Mikhail Dudarev on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct KeychainPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
